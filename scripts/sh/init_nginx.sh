173f264f4357593a15b040bd952f109fb8ac51e1
patrickfrey <patrickpfrey@yahoo.com>
2015-07-08 13:38:10 +0200

    ...

---
+++ scripts/sh/init_nginx.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+function clean_up
+{
+	echo "shut down";
+}
+
+trap clean_up HUP INT QUIT KILL TERM
+
+case "$1" in
+	"start")
+		echo "Starting nginx"
+		service php5-fpm start
+		service nginx start
+	;;
+	"")
+		echo "Usage: docker run start" 
+		exit
+	;;
+esac
+
