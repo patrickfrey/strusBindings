c2ae5f243eb81ee53c1c83b3db9b8c7ba557c598
patrickfrey <patrickpfrey@yahoo.com>
2015-08-04 17:13:03 +0200

    v0.1.6

--- src/version.hpp
+++ src/version.hpp
@@ -35,10 +35,10 @@ namespace strus
 #define STRUS_BINDINGS_VERSION (\
 	0 * 1000000\
 	+ 1 * 10000\
-	+ 5\
+	+ 6\
 )
 
-#define STRUS_BINDINGS_VERSION_STRING "0.1.5"
+#define STRUS_BINDINGS_VERSION_STRING "0.1.6"
 
 }//namespace
 #endif
