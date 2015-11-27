7958a43a2483919aeffc91048b8e77fb8a13b002
Andreas Baumann <mail@andreasbaumann.cc>
2015-11-27 10:43:54 +0100

    fixed Python building on ArchLinux OBS

--- cmake/boost.cmake
+++ cmake/boost.cmake
@@ -1,12 +1,9 @@
 set(Boost_USE_MULTITHREADED ON)
 find_package( Boost 1.53.0 COMPONENTS atomic QUIET )
 if( Boost_ATOMIC_FOUND )
-	find_package( Boost 1.53.0 REQUIRED COMPONENTS thread system date_time atomic )
+	find_package( Boost 1.53.0 REQUIRED COMPONENTS thread system date_time atomic python )
 else()
-	find_package( Boost 1.53.0 REQUIRED COMPONENTS thread system date_time )
+	find_package( Boost 1.53.0 REQUIRED COMPONENTS thread system date_time python )
 endif()
-
-# HACK: Because find package for boost_python does not work at least on Ubuntu we have to define the Boost_Python_LIBRARIES (currently debian only):
-set( Boost_Python_LIBRARIES boost_python-py27 )
 
 
