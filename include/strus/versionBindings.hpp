cde6ae79df2806b69ae765d21b66376937566a82
patrickfrey <patrickpfrey@yahoo.com>
2017-04-22 20:23:28 +0200

    some cleanup, add Lua libraries

---
+++ include/strus/versionBindings.hpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2014 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#ifndef _STRUS_BINDINGS_VERSION_HPP_INCLUDED
+#define _STRUS_BINDINGS_VERSION_HPP_INCLUDED
+
+namespace strus
+{
+
+#define STRUS_BINDINGS_VERSION (\
+	0 * 1000000\
+	+ 14 * 10000\
+	+ 0\
+)
+
+#define STRUS_BINDINGS_VERSION_STRING "0.14.0"
+
+}//namespace
+#endif
