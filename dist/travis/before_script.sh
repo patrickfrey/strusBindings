5b4955c3980ac8e491fbabffd7a6fc5711838ac5
patrickfrey <patrickpfrey@yahoo.com>
2017-12-17 10:31:24 +0100

    try to install PHP7 from the original package repository

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -14,12 +14,8 @@ case $OS in
 			libatlas-dev \
 			liblapack-dev \
 			libblas-dev \
-			python3-dev
-		sudo apt-get install -y language-pack-en-base
-		sudo locale-gen en_US.UTF-8
-		sudo LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
-		sudo LC_ALL=en_US.UTF-8 apt-get update
-		sudo LC_ALL=en_US.UTF-8 apt-get install -y php7.0 php7.0-dev
+			python3-dev \
+			php7.0-dev
 		;;
 
 	Darwin)
