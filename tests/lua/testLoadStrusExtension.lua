7d71ac497176201bcf8698165c4a8588e64c9698
patrickfrey <patrickpfrey@yahoo.com>
2017-12-27 15:26:10 +0100

    cmake build: add flag '-undefined dynamic_lookup' for linking python3 module on OSX

--- tests/lua/testLoadStrusExtension.lua
+++ tests/lua/testLoadStrusExtension.lua
@@ -1,4 +1,5 @@
-require "strus"
+-- require "strus" 
+-- ... Loading strus with the Lua module "strus.lua" is only needed when running the test with the official Lua interpreter and not with strusLua
 
 local ctxconfig = { trace = { log = "dump", file = "stdout" } }
 local ctx = strus_Context.new( ctxconfig )
