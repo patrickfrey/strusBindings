fefbfc419819216da336885cf7fe87fb822e0545
patrickfrey <patrickpfrey@yahoo.com>
2018-01-13 19:39:08 +0100

    updated travis before build scripts: install boost 1.58 to satisfy changed requirement of boost 1.57 instead of 1.53

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -9,12 +9,14 @@ case $OS in
 		sudo apt-get update -qq
 		sudo apt-get install -y \
 			cmake \
-			libboost-all-dev \
 			libleveldb-dev \
 			libatlas-dev \
 			liblapack-dev \
 			libblas-dev \
 			python3-dev
+		sudo add-apt-repository -y ppa:kojoley/boost
+		sudo apt-get -q update
+		sudo apt-get install libboost-atomic1.58-dev libboost-thread1.58-dev libboost-system1.58-dev libboost-filesystem1.58-dev libboost-regex1.58-dev
 		sudo apt-get install -y language-pack-en-base
 		sudo locale-gen en_US.UTF-8
 		sudo LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
