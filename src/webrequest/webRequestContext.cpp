259f77474f1701458475ba343ca63cfff6bdc30f
PatrickFrey <patrickpfrey@yahoo.com>
2018-10-25 11:44:04 +0200

    added test script

--- src/webrequest/webRequestContext.cpp
+++ src/webrequest/webRequestContext.cpp
@@ -1418,3 +1418,4 @@ DONE:
 	return true;
 }
 
+
---
+++ tests/python3/testInsert_Issue46.py
@@ -0,0 +1,30 @@
+import strus
+
+ctx = strus.Context(None)
+config = {
+    'path': 'instance/',
+    'cache': '512M',
+    'statsproc': 'default'
+}
+if ctx.storageExists(config):
+    ctx.destroyStorage(config)
+
+ctx.createStorage(config)
+storage = ctx.createStorageClient(config)
+
+transaction = storage.createTransaction()
+
+docclass = {"mimetype":"text/plain"}
+content = 'Text of doc1.'
+
+analyzer = ctx.createDocumentAnalyzer( docclass )
+analyzer.addSearchIndexFeature( "word", "", "word", "lc")
+doc = analyzer.analyzeSingle( content, docclass )
+
+print( "%s" % ctx.debug_serialize( doc))
+
+transaction.insertDocument( 'doc1', doc)
+
+transaction.commit()
+storage.close()
+
