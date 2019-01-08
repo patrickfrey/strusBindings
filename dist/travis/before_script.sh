ad526645d4e969ce0609443ebe7b9b23f4f2cf09
PatrickFrey <patrickpfrey@yahoo.com>
2019-01-08 16:02:37 +0100

    reorganized build of language bindings according to changes in papuga build

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -13,30 +13,32 @@ case $OS in
 			libatlas-dev \
 			liblapack-dev \
 			libblas-dev \
-			python3-dev
-		sudo add-apt-repository -y ppa:kojoley/boost
-		sudo apt-get -q update
-		sudo apt-get install libboost-atomic1.58-dev libboost-thread1.58-dev libboost-system1.58-dev libboost-filesystem1.58-dev libboost-regex1.58-dev
-		sudo apt-get install -y language-pack-en-base
-		sudo locale-gen en_US.UTF-8
-		sudo LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
-		sudo LC_ALL=en_US.UTF-8 apt-get update
-		sudo LC_ALL=en_US.UTF-8 apt-get install -y php7.0 php7.0-dev
+			libboost-all-dev
+		if test "x$PAPUGA_WITH_PYTHON" = "xYES"; then
+			sudo apt-get install -y python3-dev
+		fi
+		if test "x$PAPUGA_WITH_PHP" = "xYES"; then
+			sudo apt-get install -y language-pack-en-base
+			sudo locale-gen en_US.UTF-8
+			sudo LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 add-apt-repository -y ppa:ondrej/php
+			sudo LC_ALL=en_US.UTF-8 apt-get update
+			sudo LC_ALL=en_US.UTF-8 apt-get install -y php7.0 php7.0-dev
+		fi
 		;;
 
 	Darwin)
 		brew update
 		brew upgrade cmake
-		# -- brew upgrade boost
-		brew upgrade python
+		brew upgrade boost
+		if test "x$PAPUGA_WITH_PHP" = "xYES"; then
+			brew install php70 || true
+		fi
+		# make sure cmake finds the brew version of gettext
 		brew install gettext snappy leveldb || true
 		brew link --force gettext || true
 		brew link leveldb || true
 		brew link snappy || true
 		brew install lapack openblas || true
-		brew tap homebrew/homebrew-php
-		brew install php70 || true
-		# make sure cmake finds the brew version of gettext
 		;;
 
 	*)
